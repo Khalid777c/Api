0d0025e080b8bdca7ccffc8e6256b3a6085903dc
Khalid777c <khaledcode30@gmail.com>
2025-02-22 12:59:09 +0200

    Add files via upload

---
+++ Api.js
@@ -0,0 +1,180 @@
+// ملف بيانات
+const data = [
+  {
+    name: "Mohamed Ali",
+    age: 30,
+    job: "Web Developer",
+  },
+  {
+    name: "Ahmed Mohamed",
+    age: 25,
+    job: "Graphic Designer",
+  },
+  {
+    name: "Fatma Khaled",
+    age: 28,
+    job: "Marketing Manager",
+  },
+  {
+    name: "Omar Abdullah",
+    age: 35,
+    job: "Software Engineer",
+  },
+  {
+    name: "Reem Khaled",
+    age: 22,
+    job: "Mobile App Developer",
+  },
+  {
+    name: "Amir Hussein",
+    age: 40,
+    job: "Project Manager",
+  },
+  {
+    name: "Yasmin Ali",
+    age: 26,
+    job: "Web Designer",
+  },
+  {
+    name: "Mohamed Amin",
+    age: 32,
+    job: "Web Developer",
+  },
+  {
+    name: "Sara Khaled",
+    age: 29,
+    job: "Marketing Manager",
+  },
+  {
+    name: "Abdullah Omar",
+    age: 38,
+    job: "Software Engineer",
+  },
+  {
+    name: "Reem Abdullah",
+    age: 24,
+    job: "Mobile App Developer",
+  },
+  {
+    name: "Mohamed Galal",
+    age: 41,
+    job: "Project Manager",
+  },
+  {
+    name: "Yasmin Mohamed",
+    age: 27,
+    job: "Web Designer",
+  },
+  {
+    name: "Amir Ali",
+    age: 33,
+    job: "Web Developer",
+  },
+  {
+    name: "Sara Ali",
+    age: 30,
+    job: "Marketing Manager",
+  },
+  {
+    name: "Abdullah Mohamed",
+    age: 39,
+    job: "Software Engineer",
+  },
+  {
+    name: "Reem Ali",
+    age: 25,
+    job: "Mobile App Developer",
+  },
+  {
+    name: "Mohamed Hussein",
+    age: 42,
+    job: "Project Manager",
+  },
+  {
+    name: "Yasmin Hussein",
+    age: 28,
+    job: "Web Designer",
+  },
+  {
+    name: "Amir Hussein",
+    age: 34,
+    job: "Web Developer",
+  },
+  {
+    name: "Sara Hussein",
+    age: 31,
+    job: "Marketing Manager",
+  },
+  {
+    name: "Abdullah Hussein",
+    age: 40,
+    job: "Software Engineer",
+  },
+  {
+    name: "Reem Hussein",
+    age: 26,
+    job: "Mobile App Developer",
+  },
+  {
+    name: "Mohamed Galal",
+    age: 43,
+    job: "Project Manager",
+  },
+  {
+    name: "Yasmin Galal",
+    age: 29,
+    job: "Web Designer",
+  },
+  {
+    name: "Amir Galal",
+    age: 35,
+    job: "Web Developer",
+  },
+  {
+    name: "Sara Galal",
+    age: 32,
+    job: "Marketing Manager",
+  },
+  {
+    name: "Abdullah Galal",
+    age: 41,
+    job: "Software Engineer",
+  },
+  {
+    name: "Reem Galal",
+    age: 27,
+    job: "Mobile App Developer",
+  },
+  {
+    name: "Mohamed Khaled",
+    age: 44,
+    job: "Project Manager",
+  },
+  {
+    name: "Yasmin Khaled",
+    age: 30,
+    job: "Web Designer",
+  },
+  {
+    name: "Amir Khaled",
+    age: 36,
+    job: "Web Developer",
+  },
+  {
+    name: "Sara Khaled",
+    age: 33,
+    job: "Marketing Manager",
+  },
+  {
+    name: "Abdullah Khaled",
+    age: 42,
+    job: "Software Engineer",
+  },
+  {
+    name: "Reem Khaled",
+    age: 28,
+    job: "Mobile App Developer",
+  },
+];
+
+export default data;
